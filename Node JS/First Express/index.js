8163622b37c2935393b1086a32e843fff028784a
Finalcreature <finalnadav@gmail.com>
2022-02-03 15:02:52 +0200

    EJS setup

--- Node JS/First Express/index.js
+++ Node JS/First Express/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.listen(3000, () =>
 {
     console.log('Listening on port 3000')
 })
 
 
+app.get('/' ,(req,res) =>{
+    res.render('home.ejs')
+})
 
 app.get('/search' ,(req,res) =>{
     const {q} = req.query
