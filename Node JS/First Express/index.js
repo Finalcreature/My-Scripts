ec090f26ac02db58a4900cf9514b2304f8fac440
Finalcreature <finalnadav@gmail.com>
2022-02-24 14:12:58 +0200

    Redirecting and Patching comments

--- Node JS/First Express/index.js
+++ Node JS/First Express/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { userInfo, type } = require('os');
 const app = express();
 const path = require('path');
+const {v4 : uuid} = require('uuid')
+
+const methodOvveride = require('method-override')
+
 
 const wineries = require('./data/winery.json')
 const comments = require('./data/commentsDB.json');
@@ -15,6 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({ extended: true }))
 // app.use(express.json)
+
+app.use(methodOvveride('_method'))
 
 
 
@@ -61,10 +66,31 @@ app.get('/comments/new',(req,res)=>{
 })
 
 app.post('/comments',(req,res)=>{
-    console.log(req.body)
     const {username,comment} = req.body
-    comments.comments.push({username,comment})
-    res.render('/comments/index',comments)
+    comments.comments.push({username,comment, id: uuid()})
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id',(req,res)=>{
+   const {id} = req.params
+   const comment = comments.comments.find(c => c.id == id.toString())
+   
+   res.render('comments/show', {comment})
+})
+
+app.get('/comments/:id/edit',(req,res)=>{
+    const {id} = req.params
+    const comment = comments.comments.find(c => c.id == id.toString())
+    
+    res.render('comments/update', {comment})
+ })
+
+app.patch('/comments/:id',(req,res)=>{
+    const {id} = req.params
+    const foundComment = comments.comments.find(c => c.id == id.toString())
+    const newCommentText = req.body.comment
+    foundComment.comment = newCommentText
+    res.redirect('/comments')
 })
 
 app.get('/people',(req,res) =>{
