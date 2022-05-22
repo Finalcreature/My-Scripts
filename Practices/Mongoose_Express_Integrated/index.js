32083cbe832c3cc233c6dc2ca261e7977673f781
Finalcreature <finalnadav@gmail.com>
2022-05-22 16:21:04 +0300

    Farm stand completed

--- Practices/Mongoose_Express_Integrated/index.js
+++ Practices/Mongoose_Express_Integrated/index.js
@@ -26,39 +26,45 @@ app.listen(3000, ()=>{
     console.log("App IS LISTENING ON PORT 3000!")
 })
 
-
 //variables
 
 const categories = ['fruit','vegetable', 'dairy']
 
-
-
-
-
-
 //routes
 
+//General/Filtered
 app.get('/products', async (req,res) =>{
-    const products = await Product.find({})
-    res.render('products/index', {products})
+    const {category} = req.query
+    if(category) {
+        const products = await Product.find({category})
+        res.render('products/index', {products, category})
+    }
+    else{
+        const products = await Product.find({})
+        res.render('products/index', {products, category: 'All'})
+    } 
 }) 
 
+//New item page
 app.get('/products/new', (req,res) =>{
     res.render('products/new', {categories})
 }) 
 
+//Update page
 app.get('/products/:id/edit', async (req,res) =>{
     const {id} = req.params
     const product = await Product.findById(id)
     res.render('products/edit', {product, categories})
 }) 
 
+//Update item
 app.put('/products/:id', async (req, res) =>{
     const {id} = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
     res.redirect(`/products/${product._id}`)
 })
 
+//Add new
 app.post('/products', async (req,res) => {
     const newProduct = new Product(req.body)
     await newProduct.save()
@@ -66,17 +72,17 @@ app.post('/products', async (req,res) => {
     res.redirect(`/products/${newProduct._id}`)
 })
 
-
+//Item details
 app.get('/products/:id', async (req,res) =>{
     const {id} = req.params
     const product = await Product.findById(id)
     res.render('products/show', {product})
 }) 
 
+//Delete item
 app.delete('/products/:id', async (req,res) =>{
     const {id} = req.params
     await Product.findByIdAndDelete(req.params.id)
     res.redirect('/products')
 })
-
 
