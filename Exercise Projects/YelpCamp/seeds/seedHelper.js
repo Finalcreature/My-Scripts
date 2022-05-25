3db9e58f294b6d2d2607a8e8c2c9504f53b67cd3
Finalcreature <finalnadav@gmail.com>
2022-05-25 18:38:30 +0300

    Starting YelpCamp

---
+++ Exercise Projects/YelpCamp/seeds/seedHelper.js
@@ -0,0 +1,17 @@
+module.exports.descriptors = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade'
+]
+
+module.exports.places = [
+    'Flats',
+    'Village',
+    'Canyon',
+    'Swamp',
+    'Mountain',
+    'Sea',
+    'Pond'
+]
